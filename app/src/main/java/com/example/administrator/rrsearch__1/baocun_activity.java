f3f9c1d7e50fce3128f3255f09733c96a644ac91
shuduxu <niuniu82shudu@163.com>
2020-11-02 19:13:43 +0800

    values、pictures

---
+++ app/src/main/java/com/example/administrator/rrsearch__1/baocun_activity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.rrsearch__1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class baocun_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.baocun_activity);
+    }
+}
